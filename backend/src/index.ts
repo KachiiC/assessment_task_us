208425768047d05c0f7f7a2182ced4682015961b
Ka Cheok Cheong <kacheokcheong@example.com>
2024-08-20 08:15:19 +0100

    KC: add overload protection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,19 @@
 import express, { Express, json } from "express";
 import cors from "cors";
+import protect from 'overload-protection'
 import dotenv from "dotenv";
 import router from "./router";
+
+const protectionConfig = {
+  production: false, 
+  clientRetrySecs: 1, 
+  sampleInterval: 5, 
+  maxEventLoopDelay: 42, 
+  maxHeapUsedBytes: 0, 
+  maxRssBytes: 0,
+  errorPropagationMode: false,
+  logging: false
+}
 
 dotenv.config();
 
@@ -11,6 +23,7 @@ const port = process.env.PORT || 8000;
 app.use(json());
 app.use(cors());
 app.use(router);
+app.use(protect("express", protectionConfig));
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
